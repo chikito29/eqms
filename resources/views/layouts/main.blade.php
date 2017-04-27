047198a6b311df7c4215275e2bdc298d9baf8415
VisualTrauma <deguzman.esmeraldo@gmail.com>
2017-04-27 16:08:05 +0800

    disabled copying of content in browser

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -92,6 +92,24 @@
                     noty({text: '{{ $notify['message'] }}', layout: 'topRight', type: '{{ $notify['type'] }}'});
                 @endif
             });
+
+            $("body").on("contextmenu",function(e){
+                return false;
+            });
+
+            $('body').bind('cut copy paste', function (e) {
+                e.preventDefault();
+            });
+
+            $(window).bind('keydown', function(event) {
+                if (event.ctrlKey || event.metaKey) {
+                    switch (String.fromCharCode(event.which).toLowerCase()) {
+                        case 's':
+                            event.preventDefault();
+                            break;
+                    }
+                }
+            });
         </script>
         <!-- END THIS PAGE PLUGINS-->
 
