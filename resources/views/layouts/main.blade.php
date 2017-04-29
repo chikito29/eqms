ab783c74fbf7e097912cc53e2303592831b02d7b
VisualTrauma <deguzman.esmeraldo@gmail.com>
2017-04-29 11:40:27 +0800

    added csrf metadata

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="icon" href="{{ url('favicon.ico') }}" type="image/x-icon" />
         <!-- END META SECTION -->
 
