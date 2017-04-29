614d5daa93b8aa895d8dcc7ae9bcaf04bf667990
VisualTrauma <deguzman.esmeraldo@gmail.com>
2017-04-29 14:26:14 +0800

    deleted public function create

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -31,15 +31,6 @@ class LogController extends Controller {
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create() {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request $request
