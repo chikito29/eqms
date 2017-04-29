abfab0d1571a24e129e8dd707d506206344944b5
VisualTrauma <deguzman.esmeraldo@gmail.com>
2017-04-29 14:50:49 +0800

    added log feature on AccessRequestController

--- app/Http/Controllers/AccessRequestController.php
+++ app/Http/Controllers/AccessRequestController.php
@@ -16,6 +16,12 @@ class AccessRequestController extends Controller
     }
 
     public function index() {
+        Make::log(
+            'visited Access Requests index',
+            $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'],
+            $_SERVER['REMOTE_ADDR']
+        );
+
         $accessRequests = AccessRequest::where('status', 'Pending')->orWhere('status', 'Granted')->paginate(5);
         return view ('accessrequests.index', compact('accessRequests'));
     }
@@ -28,6 +34,14 @@ class AccessRequestController extends Controller
         $accessRequest->user_name = $request->user_name;
         $accessRequest->purpose = $request->purpose;
         $accessRequest->save();
+
+        $user = collect(NA::user($accessRequest->user_id));
+        Make::log(
+            $user['first_name'] .' '. $user['last_name'] .' '. 'requested an access for on eQMS for ' . $accessRequest->purpose,
+            $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'],
+            $_SERVER['REMOTE_ADDR']
+        );
+
         return redirect('pending');
     }
 
@@ -35,7 +49,15 @@ class AccessRequestController extends Controller
         $accessRequest = AccessRequest::find($id);
         $accessRequest->fill(['status' => 'Denied'])->save();
         $accessRequest->delete();
+
         session()->flash('notify', ['message' => 'Request has been denied.', 'type' => 'success']);
+        $user = collect(NA::user($accessRequest->user_id));
+        Make::log(
+            request('user.first_name') .' '. request('user.last_name') . ' denied ' . $user['first_name'] .' '. $user['last_name'] .'\''. ' access request',
+            $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'],
+            $_SERVER['REMOTE_ADDR']
+        );
+
         return redirect()->route('access-requests.index');
     }
 
@@ -45,6 +67,12 @@ class AccessRequestController extends Controller
         if (NA::grantTemporaryAccess($accessRequest->user_id, $request->duration)) {
             $accessRequest->fill(['status' => 'Granted'])->save();
             session()->flash('notify', ['message' => 'Access has been granted.', 'type' => 'success']);
+            $user = collect(NA::user($accessRequest->user_id));
+            Make::log(
+                request('user.first_name') .' '. request('user.last_name') . ' granted ' . $user['first_name'] .' '. $user['last_name'] .'\''. ' access request',
+                $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'],
+                $_SERVER['REMOTE_ADDR']
+            );
         } else {
             session()->flash('notify', ['message' => 'Something went wrong!.', 'type' => 'danger']);
         }
@@ -58,6 +86,12 @@ class AccessRequestController extends Controller
             $accessRequest->fill(['status' => 'Revoked'])->save();
             $accessRequest->delete();
             session()->flash('notify', ['message' => 'Access has been revoked.', 'type' => 'success']);
+            $user = collect(NA::user($accessRequest->user_id));
+            Make::log(
+                request('user.first_name') .' '. request('user.last_name') . ' revoked ' . $user['first_name'] .' '. $user['last_name'] .'\''. ' access request',
+                $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'],
+                $_SERVER['REMOTE_ADDR']
+            );
         } else {
             session()->flash('notify', ['message' => 'Something went wrong!.', 'type' => 'danger']);
         }
